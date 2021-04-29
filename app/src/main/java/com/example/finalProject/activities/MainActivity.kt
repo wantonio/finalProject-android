f894712771473c2959a3bc4c7d6f573a12a051fc
Walter Calderon Jimenez <wcalderon9891@gmail.com>
2021-04-29 12:05:38 -0600

    agregar color rojo, agregar main fragment y mover mainactivity

--- app/src/main/java/com/example/finalProject/activities/MainActivity.kt
+++ app/src/main/java/com/example/finalProject/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.finalProject
+package com.example.finalProject.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.finalProject.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
