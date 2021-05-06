a4327b2c4686a5009cc9ffa856b94343b2aa866e
Randall Portuguez Hdz <57552205+rportu@users.noreply.github.com>
2021-05-06 17:15:53 -0600

    barra de busqueda

---
+++ app/src/main/java/com/example/finalProject/adapter/PokemonAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.finalProject.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.finalProject.models.Contact
+import com.example.finalProject.databinding.ListCellBinding
+import com.squareup.picasso.Picasso
+
+class PokemonAdapter : RecyclerView.Adapter<PokemonAdapter.PokemonViewHolder>() {
+    private var _binding: ListCellBinding? = null
+    private val binding get() = _binding!!
+    lateinit var clickListener: (pokemonId: Int) -> Unit
+
+    var contacts: List<Contact> = emptyList()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    inner class PokemonViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(name: String, imageUrl: String) {
+            binding.textView.text = name
+            Picasso.get().load(imageUrl).into(binding.imageView)
+            itemView.setOnClickListener {
+                clickListener(2)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonViewHolder {
+        _binding = ListCellBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PokemonViewHolder(binding.root)
+    }
+
+    override fun onBindViewHolder(holder: PokemonViewHolder, position: Int) {
+        val contact = contacts[position]
+        holder.bind(contact.name, contact.imageURL)
+    }
+
+    override fun getItemCount(): Int = contacts.size
+
+    fun setOnItemClickListener(clickListener: (id: Int) -> Unit) {
+        this.clickListener = clickListener
+    }
+}
