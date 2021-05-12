a846eeb3e1efb42260b623cacb05f9bfcd81f3e3
Randall Portuguez Hdz <57552205+rportu@users.noreply.github.com>
2021-05-12 00:33:58 -0600

    caida del app en lista de pokemones

---
+++ app/src/main/java/com/example/finalProject/api/APIServiceList.kt
@@ -0,0 +1,4 @@
+package com.example.finalProject.api
+
+interface APIServiceList {
+}
---
+++ app/src/main/java/com/example/finalProject/viewmodels/PokemonesListViewM.kt
@@ -0,0 +1,58 @@
+package com.example.finalProject.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.finalProject.api.APIServiceList
+import com.example.finalProject.models.Pokemon
+import com.example.finalProject.models.PokemonList
+import com.example.finalProject.models.PokemonListDetail
+import com.example.finalProject.models.PokemonListResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class PokemonesListVM : ViewModel(){
+
+
+    private val PokemonListR = MutableLiveData<List<PokemonListDetail>>()
+    private var service: APIServiceList
+
+    init {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://pokeapi.co/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        service = retrofit.create(APIServiceList::class.java)
+    }
+
+    fun makeAPIRequest(){
+        service.getAllPokemons(limit = 15)
+            .enqueue(object : Callback<PokemonListResponse> {
+                override fun onResponse(call: Call<PokemonListResponse>, response: Response<PokemonListResponse>) {
+                    response.body()?.let {
+                        PokemonListR.postValue(it.list)
+                    }
+                }
+
+                override fun onFailure(call: Call<PokemonListResponse>, t: Throwable) {
+                    val a = ""
+                }
+
+            })
+
+    }
+
+    fun getPokemonList() : LiveData<List<PokemonListDetail>>{
+        return PokemonListR
+
+    }
+
+
+
+
+
+}
